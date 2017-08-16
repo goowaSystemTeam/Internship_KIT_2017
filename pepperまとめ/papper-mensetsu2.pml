c8bf60b4ba1cb28244454b610ebe5827e622842b
sasashun <30799750+sasashun@users.noreply.github.com>
2017-08-16 17:17:56 +0900

    成果物
    成果物提出します

---
+++ pepperまとめ/papper-mensetsu2.pml
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="papper-mensetsu2" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs>
+        <Dialog name="hajimenoaisatsu" src="hajimenoaisatsu/hajimenoaisatsu.dlg" />
+        <Dialog name="sibouriyu" src="sibouriyu/sibouriyu.dlg" />
+        <Dialog name="sibouriyu2" src="sibouriyu2/sibouriyu2.dlg" />
+        <Dialog name="syuuryou" src="syuuryou/syuuryou.dlg" />
+        <Dialog name="owari" src="owari/owari.dlg" />
+        <Dialog name="1.2" src="1.2/1.2.dlg" />
+        <Dialog name="1.4" src="1.4/1.4.dlg" />
+        <Dialog name="bunki" src="bunki/bunki.dlg" />
+        <Dialog name="gouhi" src="gouhi/gouhi.dlg" />
+        <Dialog name="hugoukaku" src="hugoukaku/hugoukaku.dlg" />
+        <Dialog name="owari2" src="owari2/owari2.dlg" />
+    </Dialogs>
+    <Resources />
+    <Topics>
+        <Topic name="hajimenoaisatsu_jpj" src="hajimenoaisatsu/hajimenoaisatsu_jpj.top" topicName="hajimenoaisatsu" language="ja_JP" />
+        <Topic name="sibouriyu_jpj" src="sibouriyu/sibouriyu_jpj.top" topicName="sibouriyu" language="ja_JP" />
+        <Topic name="sibouriyu2_jpj" src="sibouriyu2/sibouriyu2_jpj.top" topicName="sibouriyu2" language="ja_JP" />
+        <Topic name="syuuryou_jpj" src="syuuryou/syuuryou_jpj.top" topicName="syuuryou" language="ja_JP" />
+        <Topic name="owari_jpj" src="owari/owari_jpj.top" topicName="owari" language="ja_JP" />
+        <Topic name="1.2_jpj" src="1.2/1.2_jpj.top" topicName="1.2" language="ja_JP" />
+        <Topic name="1.4_jpj" src="1.4/1.4_jpj.top" topicName="1.4" language="ja_JP" />
+        <Topic name="bunki_jpj" src="bunki/bunki_jpj.top" topicName="bunki" language="ja_JP" />
+        <Topic name="gouhi_jpj" src="gouhi/gouhi_jpj.top" topicName="gouhi" language="ja_JP" />
+        <Topic name="hugoukaku_jpj" src="hugoukaku/hugoukaku_jpj.top" topicName="hugoukaku" language="ja_JP" />
+        <Topic name="owari2_jpj" src="owari2/owari2_jpj.top" topicName="owari2" language="ja_JP" />
+    </Topics>
+    <IgnoredPaths />
+    <Translations auto-fill="ja_JP">
+        <Translation name="translation_ja_JP" src="translations/translation_ja_JP.ts" language="ja_JP" />
+    </Translations>
+</Package>
---
+++ pepperまとめ/translations/translation_ja_JP.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>こんにちは</source>
+            <comment>Text</comment>
+            <translation type="vanished">こんにちは</translation>
+        </message>
+        <message>
+            <source>はいですね</source>
+            <comment>Text</comment>
+            <translation type="obsolete">はいですね</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>こんにちは</source>
+            <comment>Text</comment>
+            <translation type="vanished">こんにちは</translation>
+        </message>
+        <message>
+            <source>いいえですね</source>
+            <comment>Text</comment>
+            <translation type="obsolete">いいえですね</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>返事がありませんね</source>
+            <comment>Text</comment>
+            <translation type="obsolete">返事がありませんね</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/batu/Say</name>
+        <message>
+            <source>不合格です</source>
+            <comment>Text</comment>
+            <translation type="obsolete">不合格です</translation>
+        </message>
+        <message>
+            <source>結果は不合格です</source>
+            <comment>Text</comment>
+            <translation type="obsolete">結果は不合格です</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/batu/Say (1)</name>
+        <message>
+            <source>結果は不合格です</source>
+            <comment>Text</comment>
+            <translation type="obsolete">結果は不合格です</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/batu/Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>結果は不合格です</source>
+            <comment>Text</comment>
+            <translation type="unfinished">結果は不合格です</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/batu/Say (3)</name>
+        <message>
+            <source>結果は不合格です</source>
+            <comment>Text</comment>
+            <translation type="obsolete">結果は不合格です</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/maru/Say</name>
+        <message>
+            <source>合格です</source>
+            <comment>Text</comment>
+            <translation type="obsolete">合格です</translation>
+        </message>
+        <message>
+            <source>結果は合格です</source>
+            <comment>Text</comment>
+            <translation type="obsolete">結果は合格です</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/maru/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>結果は合格です</source>
+            <comment>Text</comment>
+            <translation type="unfinished">結果は合格です</translation>
+        </message>
+        <message>
+            <source>結果は不合格です</source>
+            <comment>Text</comment>
+            <translation type="obsolete">結果は不合格です</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/maru/Say (2)</name>
+        <message>
+            <source>結果は合格です</source>
+            <comment>Text</comment>
+            <translation type="obsolete">結果は合格です</translation>
+        </message>
+    </context>
+</TS>
